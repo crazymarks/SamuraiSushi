b0698cc1790b990e054cc49b0d5cb12612e6d112
crazymarks <crazymarks@live.com>
2018-03-15 16:16:04 +0900

    町人の作成 町人の生成（位置未修正） 行列システムの作成 OutZoneとMiddleZoneの範囲が未修正

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -4,22 +4,198 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+    //people location
+    private Vector2 PeopleCreatePoint;
+    public float MaxY = 5f;        // screen`s max y  To define people`s create point 
+    public float MaxX = 12f;       //screen`s max x   
+    public float TopCreateVar = 4f; // define top create-zone
+    public GameObject MiddleZone;
+
+    //people location
+
+    //about people
+    public GameObject Man;           //一般人型 
+
+    private GameObject People;          //the name of every human to manage them
+    private int CreateDirection = 0;    //0=up 1=left 2=right
+    private int PeopleCount = 0;        //give every people a number to make a list
+
+    private float PeopleCreateSpeed; //the speed of people-creating
+    private int PeopleKilling;       //the amount of killed people
+
+    private List<string> CustomerList = new List<string>();
+    private float PofCustomers = 0.8f;  // the probability of be a customer  range from 0 to 1 
+    //about people
+
+    //about Sushi
+    //about Sushi
+
+
+    //about point and eating sushi
+    public Text MoneyText;
+    public Text PeoplekillText; 
+    private int Money = 0;            //dont forget to intialize it! 
+    private bool CustomerFlag = false;
+
+    //about point and eating sushi
 
 
 
+    //about ninja
+    //about ninja
 
 
+    //about fish
+    //about fish
+
+ 
 
 
+//------------------------------fuction--------------------------------------------------------------------
 
 
 	// Use this for initialization
 	void Start () {
-		
+        create_people();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        customers_list_check();
+    }
+
+    /// <summary>
+    /// change state with different popular value
+    /// </summary>
+    void popular_check()
+    {
+
+    }
+
+    /// <summary>
+    /// create fish (define amount/type/point/etc.)
+    /// </summary>
+    void create_fish()
+    {
+
+    }
+
+    /// <summary>
+    ///  create people(amount/type/point/etc.)
+    /// </summary>
+    void create_people()
+    {
+        //addpeople choose(uncomplete)
+        //addpeople choose(uncomplete)
+
+
+        int i = Random.Range(0, 3);
+        if (i == 0)      //create at up
+        {
+            float x = Random.Range(MiddleZone.transform.position.x - TopCreateVar, 
+                MiddleZone.transform.position.x + TopCreateVar);
+            PeopleCreatePoint = new Vector2(x, MaxY);
+            People = Instantiate(Man, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //compare to p of being a customer
+            bool BeCustomer = false;
+            if (j < PofCustomers)               //be a customer
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);   //add this name to customers list
+            } 
+                People.SendMessage("enter_from_up",BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;            
+        }
+        else if (i == 1)   //create at left
+        {
+            float y = Random.Range(MiddleZone.transform.position.y-MiddleZone.GetComponent<BoxCollider2D>().size.y/2,
+                MiddleZone.transform.position.y+ MiddleZone.GetComponent<BoxCollider2D>().size.y / 2);
+            PeopleCreatePoint = new Vector2(-MaxX, y);
+            People = Instantiate(Man, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //compare to p of being a customer
+            bool BeCustomer = false;
+            if (j < PofCustomers)               //be a customer
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);    //add this name to customers list
+            }
+            People.SendMessage("enter_from_left",BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if(i == 2)  //create at right
+        {
+            float y = Random.Range(MiddleZone.transform.position.y - MiddleZone.GetComponent<BoxCollider2D>().size.y / 2,
+                MiddleZone.transform.position.y + MiddleZone.GetComponent<BoxCollider2D>().size.y / 2);
+            PeopleCreatePoint = new Vector2(MaxX, y);
+            People = Instantiate(Man, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //compare to p of being a customer
+            bool BeCustomer = false;
+            if (j < PofCustomers)               //be a customer
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);  //add this name to customers list
+            }
+                People.SendMessage("enter_from_right",BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+
+        PeopleCreateSpeed = Random.Range(0.3f, 2f);       //speed of create speed 
+        Invoke("create_people", PeopleCreateSpeed);
+    }
+
+    /// <summary>
+    ///  to manage the list of waiting customer
+    /// </summary>
+    /// <param name="ObjectName"></param>
+    void customers_listadd(string ObjectName)
+    {
+        CustomerList.Add(ObjectName);
+        People.SendMessage("customers_check2", (CustomerList.Count - 1));
+        Debug.Log("sent number");
+        Debug.Log(CustomerList.Count - 1);
+        Debug.Log(ObjectName);
+    }
+    /// <summary>
+    /// to check the amount of customers now  
+    /// change with popular
+    /// </summary>
+    void customers_list_check()
+    {
+        if (CustomerList.Count > 5)
+        {
+            PofCustomers = 0.0f;
+        }
+        else
+        {
+            PofCustomers = 0.8f;
+        }
+    }
+
+
+    void kill_people(string name)
+    {
+        PeopleKilling = PeopleKilling + 1;
+
+        CustomerList.Remove(name);
+            customers_manage(1);    //only change number
+       // PeoplekillText.text = ("殺人数" + PeopleKilling.ToString());
+    }
+    /// <summary>
+    /// check after (eating sushi)(int i=0)/(kill people)(int i=1)/(add list)(int i=1)
+    /// </summary>
+    void customers_manage(int i)
+    {
+        if (i == 1)
+        {
+            for (int j = 0; j < CustomerList.Count; j++)
+            {
+                GameObject CustomersTemp = GameObject.Find(CustomerList[j]);
+                CustomersTemp.SendMessage("customers_check2", j);
+            }
+        }
+    }
+
 }
--- Assets/Script/People/PeopleBase.cs
+++ Assets/Script/People/PeopleBase.cs
@@ -3,14 +3,224 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PeopleBase : MonoBehaviour {
+    protected int EnterDirection = 0;    // 0=up 1=left 2=right
+    public float MaxY = 5f;        // screen`s max y  To define people`s create point 
+    public float MaxX = 12f;       //screen`s max x   
+    public float TopCreateVar = 4f; // define top create-zone
+    protected float Speed = 0f;
+    public float MaxSpeed = 10f;
+    public float MinSpeed = 8f;
 
-	// Use this for initialization
-	protected virtual void Start () {
-		
+    GameObject GameController;
+    GameObject MiddleZone;
+    protected float MiddleZoneX;     //the position of middlezone
+    protected float MiddleZoneY;
+    protected float MiddleZoneWidth;
+    protected float MiddleZoneHeight;
+
+    protected bool IsCustomer = false;      //to know this is a customer or not
+    protected int CustomerNumber = -1;       //the number of customer
+    protected Vector2 RandomPoint = new Vector2(0,0);
+    
+    protected float ScaleRate = 0.0f;
+
+
+    // Use this for initialization
+    protected virtual void GetStart()
+    {
+        random_speed();
+        scale_with_y();
+        GameController = GameObject.Find("GameController");
+        MiddleZone = GameObject.Find("MiddleZone");
+
+        MiddleZoneWidth = MiddleZone.GetComponent<BoxCollider2D>().size.x;
+        MiddleZoneHeight = MiddleZone.GetComponent<BoxCollider2D>().size.y;
+        
+        Vector3 Pos = MiddleZone.transform.position;
+        MiddleZoneX = Pos.x;
+        MiddleZoneY = Pos.y;
 	}
 	
 	// Update is called once per frame
-	protected virtual void Update () {
-		
+	protected virtual void FixedUpdate ()
+    {
+        float Step = Speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, RandomPoint, Step);
+        scale_with_y();
 	}
+
+
+//--------------------------------enter----------------------------------------------
+    protected virtual void enter_from_up(bool BeCustomer)
+    {
+        GetStart();
+        float x = 0f;
+        float y = 0f;
+        x = Random.Range(MiddleZoneX - (MiddleZoneWidth /2),
+            MiddleZoneX+(MiddleZoneWidth/2));           
+        y = MiddleZoneY + MiddleZoneHeight / 2;
+        RandomPoint = new Vector2(x, y);
+        EnterDirection = 0; //from up
+        IsCustomer = BeCustomer;   // this is a customer
+    }
+
+    protected virtual void enter_from_left(bool BeCustomer)
+    {
+        GetStart();
+        float x = 0f;
+        float y = 0f;
+        x = MiddleZoneX - MiddleZoneWidth / 2;
+        y = Random.Range(MiddleZoneY - MiddleZoneHeight / 2,
+            MiddleZoneX + MiddleZoneHeight / 2);           
+        RandomPoint = new Vector2(x, y);
+        EnterDirection = 1; //from left
+        IsCustomer = BeCustomer;   // this is a customer
+    }
+
+    protected virtual void enter_from_right(bool BeCustomer)
+    {
+        GetStart();
+        float x = 0f;
+        float y = 0f;
+        x = MiddleZoneX + MiddleZoneWidth / 2;
+        y = Random.Range(MiddleZoneY - MiddleZoneHeight / 2,
+            MiddleZoneX + MiddleZoneHeight / 2);
+        RandomPoint = new Vector2(x, y);
+        EnterDirection = 2; //from left
+        IsCustomer = BeCustomer;   // this is a customer
+    }
+//--------------------------------enter----------------------------------------------
+
+//--------------------------------exit-----------------------------------------------    
+    protected virtual void exit_to_up()
+    {
+        float x = 0f;
+        float y = 0f;
+        x = Random.Range(MiddleZoneX - TopCreateVar,
+            MiddleZoneX + TopCreateVar);
+        y = MaxY;
+        RandomPoint = new Vector2(x, y);
+    }
+    protected virtual void exit_to_left()
+    {
+        float x = 0f;
+        float y = 0f;
+        x = -MaxX;
+        y = Random.Range(MiddleZoneY - MiddleZoneHeight / 2,
+            MiddleZoneX + MiddleZoneHeight / 2);
+        RandomPoint = new Vector2(x, y);
+    }
+    protected virtual void exit_to_right()
+    {
+        float x = 0f;
+        float y = 0f;
+        x = MaxX;
+        y = Random.Range(MiddleZoneY - MiddleZoneHeight / 2,
+            MiddleZoneX + MiddleZoneHeight / 2);
+        RandomPoint = new Vector2(x, y);
+    }
+    //--------------------------------exit-----------------------------------------------
+
+    protected virtual void state_change()
+    {
+        if (IsCustomer)
+        {
+            customers_move(CustomerNumber);
+        }else
+        {
+            if (EnterDirection == 0)                //come from up
+            {
+                float NextDirection = Random.Range(0.0f, 1.0f);
+                if (NextDirection < 0.5f)
+                {       exit_to_right(); }
+                else
+                {       exit_to_left();  }
+            }
+            else if (EnterDirection == 1)         //come from left
+            {
+                float NextDirection = Random.Range(0.0f, 1.0f);
+                if (NextDirection < 0.5)
+                {    exit_to_right();    }
+                else
+                {    exit_to_up();       }
+            }
+            else                                 //come from right
+            {
+                float NextDirection = Random.Range(0f, 1f);
+                if (NextDirection < 0.5)
+                {    exit_to_left();     }
+                else
+                {    exit_to_up();       }
+            }
+        }
+    }
+
+    /// <summary>
+    ///  to move the position of customers in list
+    /// </summary>
+    protected virtual void customers_move(int number)
+    {
+        float y = number * 0.2f - 3;
+       RandomPoint =new Vector2(0,y);
+    }
+    /// <summary>
+    /// to change customers state (leave customers list)
+    /// </summary>
+    protected virtual void customers_check1(bool beCustomers)
+    {
+        IsCustomer = beCustomers;
+        state_change();
+    }
+    protected virtual void customers_check2(int number)
+    {
+        CustomerNumber = number;
+        state_change();
+    }
+    /// <summary>
+    /// be killed anime and 
+    /// sent message to game controller to delete the list of people
+    /// </summary>
+    protected virtual void be_killed()
+    {
+        //play animation
+        //play animation
+        GameController.SendMessage("kill_people", this.name);
+        Destroy(gameObject);
+    }
+
+    //state change
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "MiddleZone")
+        {
+            state_change();
+        }
+        if (other.name == "Line")
+        {
+            if(this.transform.position.x != 0 || CustomerNumber ==0)   
+                be_killed();
+        }
+    }
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "OutZone")
+        {
+            be_killed();
+        }
+    }
+
+    protected virtual void random_speed()
+    {
+        Speed = Random.Range(MinSpeed, MaxSpeed);
+    }
+
+    protected virtual void scale_with_y()
+    {
+            ScaleRate = (this.transform.position.y-1) * (-0.06f)  +0.15f;    //y=-0.06x-0.85
+            this.transform.localScale = new Vector3(ScaleRate, ScaleRate, 1);
+
+            Vector3 Pos = this.transform.position;
+            Pos.z = 2f * Pos.y + 17f;
+            transform.position = Pos;
+    }
 }
