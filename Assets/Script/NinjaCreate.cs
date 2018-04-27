e71837a673b3b06631c853e51da08fb250e3fc4e
kousei1026 <a381200370@gmail.com>
2018-04-27 15:48:47 +0900

    壁忍者生成
    壁忍者生成（攻撃アニメ未完成）
    壁忍者（仮）図を導入

--- Assets/Script/NinjaCreate.cs
+++ Assets/Script/NinjaCreate.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class NinjaCreate : MonoBehaviour {
     public GameObject NinjaFly;
+    public GameObject NinjaWall1;
+    int ninjaCreate = 0;
     // Use this for initialization
     void Start () {
-        Invoke("create_ninjafly", 5.0f);
+        ninjaCreate = Random.Range(1, 3);
+        switch (ninjaCreate)
+        {
+            case 1:
+                create_ninjafly();
+                break;
+            case 2:
+                create_ninjaWallLeft();
+                break;
+            case 3:
+                create_ninjaWallRight();
+                break;
+        }
+      
+
     }
 
     /// <summary>
@@ -17,6 +33,27 @@ public class NinjaCreate : MonoBehaviour {
         Vector3 pos = new Vector3(Random.Range(-7f, 7f), Random.Range(2.0f, 3.0f), 23);
         Instantiate(NinjaFly, pos, Quaternion.identity);
         float NinjaFlyCreateTime = Random.Range(4.0f, 7.0f);
-        Invoke("create_ninjafly", NinjaFlyCreateTime);
+        Invoke("Start", 4.0f);
+    }
+
+    /// <summary>
+    /// 左側壁の忍者を生成
+    /// </summary>
+    void create_ninjaWallLeft()
+    {
+        Vector3 pos = new Vector3(Random.Range(-7f, -3f), Random.Range(-1.0f, 1.0f), 15);
+        Instantiate(NinjaWall1, pos, Quaternion.identity);
+        float NinjaFlyCreateTime = Random.Range(4.0f, 7.0f);
+        Invoke("Start", 4.0f);
+    }
+
+    /// 右側壁の忍者を生成
+    /// </summary>
+    void create_ninjaWallRight()
+    {
+        Vector3 pos = new Vector3(Random.Range(3f, 7f), Random.Range(-1.0f, 1.0f), 15);
+        Instantiate(NinjaWall1, pos, Quaternion.identity);
+        float NinjaFlyCreateTime = Random.Range(4.0f, 7.0f);
+        Invoke("Start", 4.0f);
     }
 }
