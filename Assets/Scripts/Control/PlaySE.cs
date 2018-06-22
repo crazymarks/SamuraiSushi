188bd377105ad1a40f16ff4885fd05a8a8f4e49f
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2018-06-22 12:50:56 +0900

    魚が切ったSEのscript

--- Assets/Scripts/Control/PlaySE.cs
+++ Assets/Scripts/Control/PlaySE.cs
@@ -4,20 +4,41 @@ using System.Collections;
 public class PlaySE : MonoBehaviour
 {
 
-    private AudioSource sound01;
-
+    private AudioSource SEmaintain;
+    public AudioClip fishsuccess01;
+    public AudioClip fishsuccess02;
+    public AudioClip fishsuccess03;
+    int N;
     void Start()
-    {
-        //AudioSourceコンポーネントを取得し、変数に格納
-        sound01 = GetComponent<AudioSource>();
-    }
+        {
+            //AudioSourceコンポーネントを取得し、変数に格納
+            SEmaintain = GetComponent<AudioSource>();
+        }
 
+    void FishSuccess()
+        {
+            N = Random.Range(1, 3);
+            if (N == 1)
+            {
+                SEmaintain.PlayOneShot(fishsuccess01);
+            }
+            if (N == 2)
+            {
+                SEmaintain.PlayOneShot(fishsuccess02);
+            }
+            if (N == 3)
+            {
+                SEmaintain.PlayOneShot(fishsuccess03);
+            }
+        }
     void Update()
     {
         //指定のキーが押されたら音声ファイル再生
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            sound01.PlayOneShot(sound01.clip);
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            FishSuccess();
         }
+        
     }
+    
 }
