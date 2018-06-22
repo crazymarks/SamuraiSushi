3f1fdb61dcffdb3164925f2c89ad421ee54f9ebd
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2018-06-22 11:20:50 +0900

    SEの導入

---
+++ Assets/Scripts/Control/PlaySE.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySE : MonoBehaviour
+{
+
+    private AudioSource sound01;
+
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //指定のキーが押されたら音声ファイル再生
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+}
