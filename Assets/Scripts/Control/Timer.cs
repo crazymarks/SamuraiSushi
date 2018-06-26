ab4c8c6016b2e1f05e7f6f5977c7f4b1672bb407
Homaru <D.K.R@hotmail.com>
2018-06-26 14:22:35 +0900

    お客移動に関するレベルデザイン作成

--- Assets/Scripts/Control/Timer.cs
+++ Assets/Scripts/Control/Timer.cs
@@ -6,9 +6,11 @@ public class Timer {
 	
 	//時間調整関数
 	public float FishChangeTime = 5.0f;		//
-	public int FishChangeFlag = 0;		//魚のデータ変化回数
-	public float PeopleChangeTime = 10.0f;	//
-	public int PeopleChangeFlag = 0;		//お客のデータ変化回数
+	public int FishChangeFlag = 0;      //魚のデータ変化回数
+    public float PeopleChangeTime1 = 10.0f;  //お客データ変化時間１、以下同
+    public float PeopleChangeTime2 = 10.0f;  //２
+    public float PeopleChangeTime3 = 10.0f;  //３
+    public int PeopleChangeFlag = 0;		//お客のデータ変化回数
 	public float NinjaChangeTime1 = 50.0f;	//忍者データ変化時間１、以下同
 	public float NinjaChangeTime2 = 70.0f;	//２
 	public float NinjaChangeTime3 = 90.0f;	//３
@@ -23,23 +25,64 @@ public class Timer {
 	
 	public void Update(){
 		CurrentTime += Time.deltaTime;
-		
-		if (CurrentTime >= FishChangeTime){
+
+        //特定時間になったら、データを変化させる、以下同
+        if (CurrentTime >= FishChangeTime){
 			if(FishChangeFlag == 0){
 				FishChangeFlag = 1;
 				Debug.Log("魚データ変更");//デバッグ用
 			}
 		}
-		
-		if (CurrentTime >= PeopleChangeTime){
+
+        //特定時間になったら、データを変化させる、以下同
+        if (CurrentTime >= PeopleChangeTime1){
 			if(PeopleChangeFlag == 0){
 				PeopleChangeFlag = 1;
-				Debug.Log("お客データ変更");//デバッグ用
-			}
-		}
-		
-		//特定時間になったら、データを変化させる、以下同
-		if (CurrentTime >= NinjaChangeTime1){
+				Debug.Log("お客データ変更1");//デバッグ用
+                                     //デフォルト初出現	10
+                                     //デフォルト出現間隔	min2 max3
+                                     //デフォルト出現率	fast0.1 normal0.7 slow0.2
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().minCreateTime = 1.5f;//お客出現間隔変更、下同
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().maxCreateTime = 2.0f;
+
+                //お客移動速度変更、下同    fast   0.2
+                //2                         normal 0.6
+                //3                         slow   0.2
+            }
+        }
+        if (CurrentTime >= PeopleChangeTime2)
+        {
+            if (PeopleChangeFlag == 1)
+            {
+                PeopleChangeFlag = 2;
+                Debug.Log("お客データ変更2");//デバッグ用
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().minCreateTime = 0.5f;//お客出現間隔変更、下同
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().maxCreateTime = 1.5f;
+
+                //お客移動速度変更、下同    fast   0.3
+                //2                         normal 0.5
+                //3                         slow   0.2
+            }
+        }
+        if (CurrentTime >= PeopleChangeTime3)
+        {
+            if (PeopleChangeFlag == 2)
+            {
+                PeopleChangeFlag = 3;
+                Debug.Log("お客データ変更3");//デバッグ用
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().minCreateTime = 0.5f;//お客出現間隔変更、下同
+                GameObject.Find("GameController").GetComponent<PeopleCreate>().maxCreateTime = 0.5f;
+
+                //お客移動速度変更、下同    fast   0.3
+                //2                         normal 0.4
+                //3                         slow   0.3
+            }
+        }
+
+
+
+        //特定時間になったら、データを変化させる、以下同
+        if (CurrentTime >= NinjaChangeTime1){
 			if(NinjaChangeFlag == 0){
 				NinjaChangeFlag = 1;
 				Debug.Log("忍者データ変更1");//デバッグ用
