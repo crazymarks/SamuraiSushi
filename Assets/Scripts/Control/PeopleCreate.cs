79f87de883cf8e84f0a404f86c8f57ce486c9644
kousei1026 <a381200370@gmail.com>
2018-06-28 17:29:04 +0900

    女の客を追加

--- Assets/Scripts/Control/PeopleCreate.cs
+++ Assets/Scripts/Control/PeopleCreate.cs
@@ -57,7 +57,7 @@ public class PeopleCreate : MonoBehaviour {
        //（未完成）町人選択の追加
 
 
-        int i = Random.Range(0, 3);
+        int i = Random.Range(0, 6);
         if (i == 0)      //上から　町人を生成
         {
             float x = Random.Range(MiddleZone.transform.position.x - TopCreateVar,
@@ -108,17 +108,119 @@ public class PeopleCreate : MonoBehaviour {
             People.SendMessage("enter_from_right", BeCustomer);
             People.name = "People" + PeopleCount;
             PeopleCount++;
+        }else if (i == 3)
+        {
+            float x = Random.Range(MiddleZone.transform.position.x - TopCreateVar,
+                MiddleZone.transform.position.x + TopCreateVar);
+            PeopleCreatePoint = new Vector2(x, 0.5f);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)               //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);   //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_up", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if (i == 4)
+        {
+            float y = Random.Range(MiddleZone.transform.position.y - MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y,
+                MiddleZone.transform.position.y + MiddleZone.GetComponent<BoxCollider2D>().size.y / -2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y);
+            PeopleCreatePoint = new Vector2(-MaxX, y);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)               //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);    //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_left", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if (i == 5)
+        {
+            float y = Random.Range(MiddleZone.transform.position.y - MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y,
+                MiddleZone.transform.position.y + MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y);
+            PeopleCreatePoint = new Vector2(MaxX, y);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f);//お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)                //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);   //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_right", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if (i == 3)
+        {
+            float x = Random.Range(MiddleZone.transform.position.x - TopCreateVar,
+                MiddleZone.transform.position.x + TopCreateVar);
+            PeopleCreatePoint = new Vector2(x, 0.5f);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)               //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);   //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_up", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if (i == 4)
+        {
+            float y = Random.Range(MiddleZone.transform.position.y - MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y,
+                MiddleZone.transform.position.y + MiddleZone.GetComponent<BoxCollider2D>().size.y / -2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y);
+            PeopleCreatePoint = new Vector2(-MaxX, y);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f); //お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)               //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);    //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_left", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
+        }
+        else if (i == 5)
+        {
+            float y = Random.Range(MiddleZone.transform.position.y - MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y,
+                MiddleZone.transform.position.y + MiddleZone.GetComponent<BoxCollider2D>().size.y / 2 + MiddleZone.GetComponent<BoxCollider2D>().offset.y);
+            PeopleCreatePoint = new Vector2(MaxX, y);
+            People = Instantiate(Woman, PeopleCreatePoint, Quaternion.identity);
+            float j = Random.Range(0.0f, 1.0f);//お客さんになる確率と比べて、小さいだったら、お客さんになる
+            bool BeCustomer = false;
+            if (j < GameController.PofCustomers)                //お客さんになる
+            {
+                BeCustomer = true;
+                customers_listadd("People" + PeopleCount);   //この町人のnameをリストに追加
+            }
+            People.SendMessage("enter_from_right", BeCustomer);
+            People.name = "People" + PeopleCount;
+            PeopleCount++;
         }
 
         PeopleCreateSpeed = Random.Range(0.3f, 2f);       //生成スピードをランダムにする
         if(isPeopleCreate==true) {
             Invoke("create_people", PeopleCreateSpeed);
         }
+
         else
         {
             create_peoplenull();
         }
-
+        
     }
 
     void create_peoplenull()
--- Assets/Scripts/People/Woman.cs
+++ Assets/Scripts/People/Woman.cs
@@ -2,15 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Woman : MonoBehaviour {
+public class Woman : PeopleBase
+{
+    public GameObject ManFrontUpperBody;
+    public GameObject ManFrontLowerBody;
+    public Sprite front;
+    public Sprite back;
+    public Sprite side;
+    Quaternion rotation = Quaternion.identity;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void after_eatsushi(int x)
+    {
+        IsCustomer = false;
+        if (x == 3)　　//毒寿司なら死ぬ
+        {
+            killed_by_poison();
+        }
+        else
+        {
+            state_change();
+        }
+    }
+
+    protected override void be_killed()
+    {
+        base.be_killed();
+        GameObject tempObject1 = Instantiate(ManFrontUpperBody, this.transform.position, Quaternion.identity);
+        tempObject1.transform.localScale = this.transform.lossyScale;
+        tempObject1.GetComponent<UpperBody>().state = GetSpriteState();
+        GameObject tempObject2 = Instantiate(ManFrontLowerBody, this.transform.position, Quaternion.identity);
+        tempObject2.transform.localScale = this.transform.lossyScale;
+        tempObject1.GetComponent<LowerBody>().state = GetSpriteState();
+    }
+
+
+
+    protected override void SpriteInitalize()
+    {
+        base.SpriteInitalize();
+        int state = GetSpriteState();
+        switch (state)
+        {
+            case (int)MoveStatus.Front:
+                this.GetComponent<SpriteRenderer>().sprite = front;   //正面に切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                this.transform.rotation = rotation;
+                return;
+            case (int)MoveStatus.Back:
+                this.GetComponent<SpriteRenderer>().sprite = back;   //背面に切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                this.transform.rotation = rotation;
+                return;
+            case (int)MoveStatus.Left:
+                this.GetComponent<SpriteRenderer>().sprite = side;   //左向きに切り替え
+                rotation.eulerAngles = new Vector3(0, 180, 0);
+                this.transform.rotation = rotation;
+                return;
+            case (int)MoveStatus.Right:
+                this.GetComponent<SpriteRenderer>().sprite = side;   //右向きに切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                this.transform.rotation = rotation;
+                return;
+        }
+
+    }
+
+    protected override void SpriteChange()
+    {
+        base.SpriteChange();
+        int state = GetSpriteState();
+        switch (state)
+        {
+            case (int)MoveStatus.Front:
+                this.GetComponent<SpriteRenderer>().sprite = front;   //正面に切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                this.transform.rotation = rotation;
+                return;
+            case (int)MoveStatus.Back:
+                this.GetComponent<SpriteRenderer>().sprite = back;   //背面に切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                return;
+            case (int)MoveStatus.Left:
+                this.GetComponent<SpriteRenderer>().sprite = side;   //左向きに切り替え
+                rotation.eulerAngles = new Vector3(0, 180, 0);
+                this.transform.rotation = rotation;
+                return;
+            case (int)MoveStatus.Right:
+                this.GetComponent<SpriteRenderer>().sprite = side;   //右向きに切り替え
+                rotation.eulerAngles = new Vector3(0, 0, 0);
+                return;
+        }
+    }
 }
