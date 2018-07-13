e963ccd30834e30a2f8824b788e6937cb6463040
Enomoto <enoohji0817@gmail.com>
2018-07-13 09:27:28 +0900

    タイトル画面(背景、「いざ参る」文字の点滅)実装

--- Assets/Scripts/Scenes/TitleScene.cs
+++ Assets/Scripts/Scenes/TitleScene.cs
@@ -3,8 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScene : MonoBehaviour {
-	
-	void Update () {
+
+    public GameObject flashText;
+    private float nextTime;
+    private float interval = 0.5f;
+
+    void Start()
+    {
+        nextTime = Time.time;
+    }
+
+    void Update () {
+
+        if(Time.time > nextTime)
+        {
+            float textAlpha = flashText.GetComponent<CanvasRenderer>().GetAlpha();
+            if(textAlpha == 1.0f)
+            {
+                flashText.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
+            }
+            else
+            {
+                flashText.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+            }
+            nextTime += interval;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             SceneManager.Instance.LoadScene(SceneManager.MainGame);
